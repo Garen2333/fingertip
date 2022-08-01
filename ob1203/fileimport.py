c203efc37a75b3a92a09a02f321a7d65fb1b26bc
Allen, Dan <dan.allen.dn@renesas.com>
2022-08-01 09:18:45 -0700

    updated script for converting the bin to js

--- ob1203/fileimport.py
+++ ob1203/fileimport.py
@@ -1,6 +1,6 @@
 # This program takes a file name as an argument (no extension!)
 # A header file will be created containing a const array of bytes in the binary
-
+# Run this file in the folder with the latest binary file.
 import os,sys,time
 
 if (len(sys.argv) == 1):
@@ -28,8 +28,8 @@ localtime = time.asctime( time.localtime(time.time()) )
 s =  "// Binary image\n"
 s += "// Generated: " + localtime + "\n"
 s += "// File name: " + file_name + ".bin\n\n"
-
-s += "\nconst "+file_name+" = [\n\t"
+s += "\nconst latest_firmware_revision = 'v1.6';" #increment this number to a new version!!!!!!
+s += "\nconst latest_firmware = [\n\t"
 
 # Convert all data bytes to a hex string
 for i in range(len(bytes_read) -1 ):
@@ -48,7 +48,7 @@ s += '0x%.2X\n' % bytes_read[len(bytes_read)-1]
 s += '];\n'
 
 # Create the output file
-with open(file_name+".js", "w") as text_file:
+with open("latest_firmware.js", "w") as text_file:
     print(s, file=text_file)
 print("\n\rConversion complete...")
 time.sleep(1)
