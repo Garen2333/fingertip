c4715288d9ea4a54533b0af861cc621ef2107470
Zhan925 <zhan2948@gmail.com>
2023-03-15 16:17:48 -0700

    swap read and write

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var packet_count = 0;
 
 // Define the CodeLess UUIDs 
 var BPP_SVC_UUID = "0000fe40-cc7a-482a-984a-7f2ed5b3e58f";
-var RX_CHAR_UUID   = "0000fe42-8e22-4541-9d4c-21edae82ed19";
-var TX_CHAR_UUID = "0000fe41-8e22-4541-9d4c-21edae82ed19";
+var RX_CHAR_UUID   = "0000fe41-8e22-4541-9d4c-21edae82ed19";
+var TX_CHAR_UUID = "0000fe42-8e22-4541-9d4c-21edae82ed19";
 
 var no_data_yet = true;
 
