2385f678a68425c934c266c3e60260f6b1e3c3c9
Yuanzhe Liu <yul156@ucsd.edu>
2023-05-31 13:14:11 -0700

    update service id and char id

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var device;
 var packet_count = 0;
 
 // Define the CodeLess UUIDs 
-var BPP_SVC_UUID = "0000fe40-cc7a-482a-984a-7f2ed5b3e58f";
-var RX_CHAR_UUID   = "0000fe42-8e22-4541-9d4c-21edae82ed19";
-var TX_CHAR_UUID = "0000fe41-8e22-4541-9d4c-21edae82ed19";
+//var BPP_SVC_UUID = "0000fe40-cc7a-482a-984a-7f2ed5b3e58f";
+var BPP_SVC_UUID = "00000000-0001-11e1-9ab4-0002a5d5c51b";
+// var RX_CHAR_UUID   = "0000fe42-8e22-4541-9d4c-21edae82ed19";
+// var TX_CHAR_UUID = "0000fe41-8e22-4541-9d4c-21edae82ed19";
+var RX_CHAR_UUID   = "00e00000-0001-11e1-ac36-0002a5d5c51b";
+var TX_CHAR_UUID = "00e00000-0001-11e1-ac36-0002a5d5c51b";
 
 var no_data_yet = true;
 
